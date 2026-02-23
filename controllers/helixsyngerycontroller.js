d065ba28e0b53c60053b0e264003ab911c3e41d5
Helix-Synergy <info.helixconferences@gmail.com>
2026-02-23 14:39:01 +0530

    Updated

--- controllers/helixsyngerycontroller.js
+++ controllers/helixsyngerycontroller.js
@@ -1,9 +1,10 @@
+const helixsynergyModel = require("../models/helixsyngerymodel");
 async function helixsynergyController(req,res){
     try {
         const {name,email,message}=req.body;
         const newEntry = new helixsynergyModel({name,email,message});
         await newEntry.save();
-        res.status(201).json({success:true,message:"Message received successfully"});
+      return  res.status(201).json({success:true,message:"Message received successfully",newEntry});
     }
     catch(error){
         console.error("Error saving synergy message:",error);
@@ -14,7 +15,7 @@ async function getAllSynergyMessages(req,res){
     try {
         const messages = await helixsynergyModel.find().sort({createdAt:-1});
 
-        res.status(200).json({success:true,data:messages});
+        res.status(200).json({success:true,data:messages,newEntry:messages});
     }
     catch(error){
         console.error("Error fetching synergy messages:",error);
